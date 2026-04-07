84c6d31bd9ef91d7661a479393aab55b4ae17a03
Mico-Code <capuz.micojohn.0664@gmail.com>
2026-04-07 16:45:21 +0800

    Letter Download Added

--- Idkman/cake-button.js
+++ Idkman/cake-button.js
@@ -1,4 +1,18 @@
 const cakebtn = document.getElementById("cake")
-cakebtn.addEventListener("click", () => {
-    window.location.href = "Birthdayletter.html"
-}); 
+if (cakebtn) {
+    cakebtn.addEventListener("click", () => {
+        window.location.href = "Birthdayletter.html"
+    });
+}
+
+const letterbtn = document.getElementById("letter1")
+if (letterbtn) {
+    letterbtn.addEventListener("click", () => {
+        const link = document.createElement("a")
+        link.href = "Birthday%20Letter/Letter.docx"
+        link.download = "Letter.docx"
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
+    });
+}
